92ef8d9e97bdedaa236e2fa09837519053f55184
Dhruv <dhruvk995@gmail.com>
2016-04-04 19:02:27 +1000

    Parameters passing into dict completed
    valid_params_dict contains required Parameters

--- seng3project/eventstudyapi/views.py
+++ seng3project/eventstudyapi/views.py
@@ -12,46 +12,31 @@ def index(request):
 def event_study_api_view(request, **kwargs):
 
     # Debug output statements
-    print(request.data)
-    print(request.FILES['stock_characteristic_file'])
-    print(request.FILES['stock_price_data_file'])
+    # print(request.data)
+    # print(request.FILES['stock_characteristic_file'])
+    # print(request.FILES['stock_price_data_file'])
 
     handle_uploaded_file(request.FILES['stock_characteristic_file'])
     handle_uploaded_file(request.FILES['stock_price_data_file'])
 
-    # TODO: Change to handle generic upper_... & lower... params
-    if request.POST.get('upper_cash_rate'):
-        print ('upper_cash_rate field is valid')
-        upper_cash_rate = request.data['upper_cash_rate']
+    # Standard dict methods do not work on the QueryDict, thus convert to a std dict
+    request_dict = dict(request.data)
+    valid_params_dict = dict()
 
-    else:
-        print('ERROR upper_cash_rate field not found')
-        # TODO: Code to return an error to the user here
+    # Iterate over the request dict, looking for valid params or files
+    for key, value in request_dict.items():
+        if key.startswith('upper_') or key.startswith('lower_'):
+            print(key, value)
+            valid_params_dict[key] = value
+        elif not (key.startswith('stock_price_data_file') or key.startswith('stock_characteristic_file')):
+            print('The following parameter is invalid: ' + str(key) + str(value))
 
-    if request.POST.get('lower_cash_rate'):
-        print ('lower_cash_rate field is valid')
-        lower_cash_rate = request.data['lower_cash_rate']
-
-    else:
-        print('ERROR lower_cash_rate field not found')
-        # TODO: Code to return an error to the user here
-
-    # Obtain upper_window and lower_window parameters
-    if request.POST.get('upper_window'):
-        print ('upper_window field is valid')
-        upper_window = request.data['upper_window']
-
-    else:
-        print('ERROR upper_window field not found')
-        # TODO: Code to return an error to the user here
-
-    if request.POST.get('lower_window'):
-        print ('lower_window field is valid')
-        lower_window = request.data['lower_window']
-
-    else:
-        print('ERROR lower_window field not found')
-        # TODO: Code to return an error to the user here
+    # Check the 2 necessary params were specified otherwise return an error
+    required_params = ['upper_window', 'lower_window']
+    for param in required_params:
+        if param not in valid_params_dict:
+            print('ERROR The following required parameter was not correctly provided:' + param)
+            # TODO: Code to return an error to the user here
 
     # serializers = ResultSerializer()
     return HttpResponse("Hello world, you are at the Event Study API index.")
