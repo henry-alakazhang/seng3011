cced50240b192c5c326c219307b18fe1e190d1f8
Damon <z5016966@zmail.unsw.edu.au>
2016-04-06 00:50:06 +1000

    Returns JSON object
    Only thing left to do now is change the date format in the object

--- seng3project/eventstudyapi/requestProcessor.py
+++ seng3project/eventstudyapi/requestProcessor.py
@@ -4,23 +4,67 @@ Created on 30 Mar 2016
 @author: Damon
 '''
 
+'''
+Created on 5 Apr 2016
+
+@author: Damon
+'''
+import re, datetime
+from eventstudyapi.parser import Parser
+from eventstudyapi.request import Request, Data
+
+
 #Process request
 def process(request):
     charsToProcess = list()
-    for stockChar in request.Data.CharInfo:
-        if (request.matchesReq(stockChar)):
-            charsToProcess.append(stockChar)
-    upperWindow = request.getUpperWindow()
-    lowerWindow = request.getLowerWindow()
-    data = request.getData()
-    result = dict()
+    for RIC in request.Data.RIC:
+        for stockChar in request.Data.CharInfo.fileData[RIC]:
+            if (request.matchesReq(stockChar)):
+                charsToProcess.append(stockChar)
+    upperWindow = int(request.upperWindow)
+    lowerWindow = int(request.lowerWindow)
+    data = request.Data
+    result = list()
     for stockChar in charsToProcess:
-        result[stockChar] = calcCumRet(stockChar,upperWindow,lowerWindow,data)
+        result.append((stockChar,calcCumRet(stockChar,upperWindow,lowerWindow,data)))
     return result
 
 #Process each individual stock characteristic
 def calcCumRet(stockChar,upperWindow,lowerWindow,data):
     cumRet = dict()
     for i in range(lowerWindow,upperWindow):
-        cumRet[i] = data.getCumRet(stockChar.getStockName(),stockChar.getDate() + i)
+        date = datetime.datetime.strptime(stockChar['Event Date'],"%d-%b-%y") + datetime.timedelta(days=i)        
+        cumRet[i] = data.getCumRet(stockChar,date.strftime("%d-%b-%y").lstrip('0'))
     return cumRet
+
+def processData(priceFile,charFile,params):
+    #parse Files
+    priceData = Parser(priceFile)
+    charData = Parser(charFile)
+    stockData = Data(priceData,charData)
+    
+    upper = re.compile("^upper_")
+    lower = re.compile("^lower_")
+    optVar = dict()
+    for param in params:
+        param_name = re.sub(re.compile("^[a-z]*_(.*)$"), '\\1', param).replace('_',' ')
+        if param_name == "window":
+            if upper.match(param):
+                upperWindow = params[param]
+            elif lower.match(param):
+                lowerWindow = params[param]
+            else:
+                raise Exception('Invalid Variable ' + param)            
+        else:
+            if param_name not in optVar:
+                optVar[param_name] = dict()
+            if upper.match(param):
+                optVar[param_name]["max"] = params[param]
+            elif lower.match(param):
+                optVar[param_name]["min"] = params[param]
+            else:        
+                raise Exception('Invalid Variable ' + param)
+    
+    request = Request(upperWindow,lowerWindow,optVar,stockData)
+    return process(request)
+    
--- seng3project/eventstudyapi/views.py
+++ seng3project/eventstudyapi/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from eventstudyapi.upload_handler import handle_uploaded_file
+from . import requestProcessor
+from eventstudyapi.parser import Parser
+from eventstudyapi.request import Request, Data
 
 
 def index(request):
@@ -13,8 +16,8 @@ def event_study_api_view(request, **kwargs):
 
     # Debug output statements
     # print(request.data)
-    # print(request.FILES['stock_characteristic_file'])
-    # print(request.FILES['stock_price_data_file'])
+    print(request.FILES.get('stock_characteristic_file'))
+    print(request.FILES.get('stock_price_data_file'))
 
     handle_uploaded_file(request.FILES['stock_characteristic_file'])
     handle_uploaded_file(request.FILES['stock_price_data_file'])
@@ -22,12 +25,17 @@ def event_study_api_view(request, **kwargs):
     # Standard dict methods do not work on the QueryDict, thus convert to a std dict
     request_dict = dict(request.data)
     valid_params_dict = dict()
-
+    
     # Iterate over the request dict, looking for valid params or files
     for key, value in request_dict.items():
+        if key.endswith('window'):
+            if key.startswith('upper_'):
+                upperWindow = value[0]
+            elif key.startswith('lower_'):
+                lowerWindow = value[0]
         if key.startswith('upper_') or key.startswith('lower_'):
             print(key, value)
-            valid_params_dict[key] = value
+            valid_params_dict[key] = value[0]
         elif not (key.startswith('stock_price_data_file') or key.startswith('stock_characteristic_file')):
             print('The following parameter is invalid: ' + str(key) + str(value))
 
@@ -37,6 +45,34 @@ def event_study_api_view(request, **kwargs):
         if param not in valid_params_dict:
             print('ERROR The following required parameter was not correctly provided:' + param)
             # TODO: Code to return an error to the user here
-
+            
+    # Process query
+    total_cum_rets = requestProcessor.processData('seng3project/media/' + str(request.FILES.get('stock_price_data_file')),'seng3project/media/' + str(request.FILES.get('stock_characteristic_file')),valid_params_dict)
+    requestResponse = convertToJson(total_cum_rets,valid_params_dict,lowerWindow,upperWindow)
     # serializers = ResultSerializer()
-    return HttpResponse("Hello world, you are at the Event Study API index.")
+    #return HttpResponse("Hello world, you are at the Event Study API index.")
+    return JsonResponse(requestResponse)
+
+def convertToJson(cumRets,params,lowerWindow,upperWindow):
+    JsonCumRets = dict()
+    JsonCumRets["parameters"] = params
+    JsonCumRets["events"] = list()
+    cumRets = sorted(cumRets, key=lambda k: k[0]['Event Date'])   
+    for chars in cumRets:
+        dateFound = False
+        indivCumRets = list()
+        for i in range(int(lowerWindow),int(upperWindow)):
+            print (i)
+            indivCumRets.append(chars[1][i])
+        for event in JsonCumRets["events"]:
+            if event["date"] == chars[0]["Event Date"]:   
+                event["returns"][chars[0]["#RIC"]] = indivCumRets       
+                dateFound = True      
+                break
+        if not dateFound:
+            event = dict()
+            event["date"] = chars[0]["Event Date"]
+            event["returns"] = dict()
+            event["returns"][chars[0]["#RIC"]] = indivCumRets   
+            JsonCumRets["events"].append(event)                
+    return JsonCumRets
