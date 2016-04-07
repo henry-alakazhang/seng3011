4a1d8d186a35ca31eaf82b889d435944532c8a0c
Dhruv <dhruvk995@gmail.com>
2016-04-07 16:42:25 +1000

    Added support for File Keys

--- eventstudyapi/views.py
+++ eventstudyapi/views.py
@@ -8,12 +8,18 @@ from rest_framework.decorators import api_view
 from . import requestProcessor
 import timeit
 import datetime
+import os
+
 
 def index(request):
     return HttpResponse("Hello world, you are at the Event Study API index.")
 
 @api_view(['POST'])
 def event_study_api_view(request, **kwargs):
+    error = ""
+    fatal = False
+
+    request_POST_dict = dict(request.POST)
 
     processing_time_start = timeit.default_timer()
     start_date_time = datetime.datetime.now()
@@ -23,17 +29,43 @@ def event_study_api_view(request, **kwargs):
     file_key = ''.join(choice(ascii_uppercase) for i in range(12))
     print (file_key)
 
+    fileFoundKey = ""
+    fileFound = False
+    if 'file_key' not in request_POST_dict:
+        error += 'ERROR There was no file_key supplied \n'
+        fatal = False
+    elif request.POST['file_key'] is '':
+        error += 'ERROR File key was none \n'
+        fatal = True
+    else:
+        # Check against existing files in media folder
+        print('In Else')
+        files_dict = list()
+        for fn in os.listdir('media/'):
+            print (str(fn))
+            files_dict.append(str(fn))
+            if fn .startswith(str(request.POST['file_key'])):
+                fileFound = True
+                fileFoundKey = str(request.POST['file_key'])
+            else:
+                print('No file match')
 
-    error = ""
-    fatal = False
-
-    files_required = ['stock_characteristic_file', 'stock_price_data_file']
-    for reqfile in files_required:
-        if reqfile not in request.FILES:
-            error += 'ERROR: File ' + reqfile + ' not proided\n'
-            fatal = True
+        if fileFound is True:
+            print('Found ' + fileFoundKey)
         else:
-            handle_uploaded_file(request.FILES[reqfile], file_key)
+            error += 'No file found with key: ' + request_POST_dict['file_key'][0] + '\n'
+            fatal = True
+
+        # file_key = request.GET['file_key']
+
+    if not fileFound:
+        files_required = ['stock_characteristic_file', 'stock_price_data_file']
+        for reqfile in files_required:
+            if reqfile not in request.FILES:
+                error += 'ERROR: File ' + reqfile + ' not proided\n'
+                fatal = True
+            else:
+                handle_uploaded_file(request.FILES[reqfile], file_key)
     
     # Standard dict methods do not work on the QueryDict, thus convert to a std dict
     request_dict = dict(request.data)
@@ -50,7 +82,8 @@ def event_study_api_view(request, **kwargs):
             print(key, value)
             valid_params_dict[key] = value[0]
         elif not (key.startswith('stock_price_data_file') or key.startswith('stock_characteristic_file')):
-            error += 'Warning: The following parameter is invalid: ' + str(key) + str(value) + '\n'
+            if not fileFound:
+                error += 'Warning: The following parameter is invalid: ' + str(key) + str(value) + '\n'
 
     # Check the 2 necessary params were specified otherwise return an error
     required_params = ['upper_window', 'lower_window']
@@ -81,14 +114,27 @@ def event_study_api_view(request, **kwargs):
 
     if fatal:
         return(HttpResponse("FATAL ERROR\n" + log))
-    
-    with open('media/' + str(file_key) + '_' + 'log.txt', 'w') as file:
-        file.write(log)
+
+    if not fileFound:
+        with open('media/' + str(file_key) + '_' + 'log.txt', 'w') as file:
+            file.write(log)
+    else:
+        with open('media/' + str(fileFoundKey) + '_' + 'log.txt', 'w') as file:
+            file.write(log)
 
     # Process query
-    total_cum_rets = requestProcessor.processData('media/' + str(file_key) + '_' + str(request.FILES.get('stock_price_data_file')), 'media/' + str(file_key) + '_' + str(request.FILES.get('stock_characteristic_file')), valid_params_dict)
+    if fileFound:
+        total_cum_rets = requestProcessor.processData('media/' + str(fileFoundKey) + '_' + str('stock_price_data_file.csv'), 'media/' + str(fileFoundKey) + '_' + str('stock_characteristic_file.csv'), valid_params_dict)
+    else:
+        total_cum_rets = requestProcessor.processData('media/' + str(file_key) + '_' + str(request.FILES.get('stock_price_data_file')), 'media/' + str(file_key) + '_' + str(request.FILES.get('stock_characteristic_file')), valid_params_dict)
+
     requestResponse = convertToJson(total_cum_rets,valid_params_dict,lowerWindow,upperWindow)
-    requestResponse['file_key'] = file_key
+
+    if not fileFound:
+        requestResponse['file_key'] = file_key
+    else:
+        requestResponse['file_key'] = fileFoundKey
+
     # serializers = ResultSerializer()
     #return HttpResponse("Hello world, you are at the Event Study API index.")
  
