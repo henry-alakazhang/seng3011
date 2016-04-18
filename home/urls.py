c319733aeb8b3fef7e661e95d913f5567ddae69c
Henry Zhang <henry.alakazhang@gmail.com>
2016-04-18 12:06:00 +1000

    Split API into multiple pages

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name="home_index"),
     url(r'^about$', views.about, name="about_us"),
     url(r'^api$', views.api_home, name="api_home"),
+    url(r'^api/versions$', views.api_versions, name="api_versions"),
+    url(r'^api/docs$', views.api_docs, name="api_docs"),
+    url(r'^api/bugs$', views.api_bugs, name="api_bugs"),
     url(r'^analytics$', views.analytics_home, name="analytics_home")
 ]
--- home/views.py
+++ home/views.py
@@ -10,5 +10,14 @@ def about(request):
 def api_home(request):
     return render(request, 'home/api.html', {'api':True});
 
+def api_versions(request):
+    return render(request, 'home/api_versions.html', {'api':True});
+
+def api_docs(request):
+    return render(request, 'home/api_docs.html', {'api':True});
+
+def api_bugs(request):
+    return render(request, 'home/api_bugs.html', {'api':True});
+
 def analytics_home(request):
     return render(request, 'home/analytics.html', {'analytics':True});
