893f8d501fb2c71c8ddffae97e754d9f864a05a6
Dhruv <dhruvk995@gmail.com>
2016-05-07 23:55:53 +1000

    Instructions for sqlLite updated

--- seng3project/settings.py
+++ seng3project/settings.py
@@ -96,8 +96,10 @@ DATABASES = {
         'PORT': '',
         # ---- Lines to comment for SQLite ----
 
+        # ---- Lines to UN-comment for SQLite ----
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # ---- Lines to UN-comment for SQLite ----
     }
 }
 
