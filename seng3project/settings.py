af70db7c22d5625126c430c01b5afe1522def2a2
Dhruv <dhruvk995@gmail.com>
2016-05-07 17:53:02 +1000

    Migrating to pgSQL

--- seng3project/settings.py
+++ seng3project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)), )
@@ -141,3 +142,5 @@ LOGIN_REDIRECT_URL = '/'
     # and are trying to access pages requiring authentication
 REGISTRATION_OPEN = True # If True, users can register
 SITE_ID = 1
+
+DATABASES['default'] =  dj_database_url.config()
