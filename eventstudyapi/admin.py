175f39032715b8eac13f6e92ce0156c2b532ffb0
Dhruv <dhruvk995@gmail.com>
2016-05-07 22:39:31 +1000

    User Portfolios added

--- eventstudyapi/admin.py
+++ eventstudyapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class UserPortFolioAdmin(admin.ModelAdmin):
+    list_display = ('id','user',  'portfolio')
+
+admin.site.register(UserPortfolio, UserPortFolioAdmin)
