cc5518135fb5c3bfd265e0c395dbb778ed390401
Dhruv <dhruvk995@gmail.com>
2016-05-07 20:02:54 +1000

    changes to get pgSQL working locally

--- seng3project/settings.py
+++ seng3project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)), )
@@ -84,8 +84,14 @@ WSGI_APPLICATION = 'seng3project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER' : 'postgres',
+        'PASSWORD': 'startx',
+        'HOST': '',
+        'PORT': '',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -143,4 +149,4 @@ LOGIN_REDIRECT_URL = '/'
 REGISTRATION_OPEN = True # If True, users can register
 SITE_ID = 1
 
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
